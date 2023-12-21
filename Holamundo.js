85be82742eee0175777bebd1e6254e0b0bd67391
Alejandro <alejotrujillo146@gmail.com>
2023-12-21 14:56:49 -0500

    Tipos de datos

--- Holamundo.js
+++ Holamundo.js
@@ -5,3 +5,31 @@ let caja = "Zapato"
 console.log(caja)
 
 const pi = 3.14
+
+//tipos de datos: Inmutables o primitivos
+let numero = 23
+numero = numero +10
+console.log(numero)
+
+let verdadero=true
+verdadero=false
+console.log(verdadero)
+
+//Tipos de datos: Mutable
+let usuario={nombre:"Carlos", edad:14}
+usuario.edad=20
+console.log(usuario)
+
+let frutas=["manzana", "pera"]
+frutas[0]="Sandias"
+console.log(frutas)
+
+function cambiarNombre (Objeto){
+    Objeto.nombre= "Nuevo nombre"
+}
+
+let persona={nombre: "Carlos"}
+cambiarNombre(persona)
+
+console.log(persona)
+
